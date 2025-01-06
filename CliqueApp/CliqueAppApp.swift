2f4d6e3f924103bbb1d89b7ecedcdd8f1d02cad3
Tarek Sakakini <tarek.sakakini@gmail.com>
2025-01-06 10:32:38 -0800

    Initial Commit

---
+++ CliqueApp/CliqueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CliqueAppApp.swift
+//  CliqueApp
+//
+//  Created by Tarek Sakakini on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CliqueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CliqueApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CliqueApp
+//
+//  Created by Tarek Sakakini on 1/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
