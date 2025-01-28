74ac46ad2f979d6973d08e5d5eec9e5755854cc7
Tarek Sakakini <tarek.sakakini@gmail.com>
2025-01-28 09:27:57 -0800

    Added friendship data

---
+++ CliqueApp/Views/MyFriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  MyFriendsView.swift
+//  CliqueApp
+//
+//  Created by Tarek Sakakini on 1/28/25.
+//
+
+import SwiftUI
+
+struct MyFriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyFriendsView()
+}
