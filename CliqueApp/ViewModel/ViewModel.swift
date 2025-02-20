ea0e2f4471c0859b076c9fd59e48a33189b67ceb
Tarek Sakakini <tarek.sakakini@gmail.com>
2025-02-20 14:29:59 -0800

    Removed a bunch of deprecated event functions

--- CliqueApp/ViewModel/ViewModel.swift
+++ CliqueApp/ViewModel/ViewModel.swift
@@ -100,24 +100,6 @@ class ViewModel: ObservableObject {
         return eventsForUser
     }
     
-    //    func getFriends(username: String) -> [String] {
-    //        if let friends = self.friendship[username] {
-    //            return friends
-    //        }
-    //        else {
-    //            return []
-    //        }
-    //    }
-    
-    //    func getFriendInvites(username: String) -> [String] {
-    //        if let invitations = self.friendInviteReceived[username] {
-    //            return invitations
-    //        }
-    //        else {
-    //            return []
-    //        }
-    //    }
-    
     func stringMatchUsers(query: String, viewingUser: UserModel, isFriend: Bool = false) -> [UserModel] {
         var to_return: [UserModel] = []
         var names_to_check: [String] = []
@@ -148,84 +130,25 @@ class ViewModel: ObservableObject {
         return to_return
     }
     
-    //    func addFriendship(username1: String, username2: String) {
-    //        if self.friendship.keys.contains(username1) {
-    //            self.friendship[username1]! += [username2]
-    //        }
-    //        else {
-    //            self.friendship[username1] = [username2]
-    //        }
-    //
-    //        if self.friendship.keys.contains(username2) {
-    //            self.friendship[username2]! += [username1]
-    //        }
-    //        else {
-    //            self.friendship[username2] = [username1]
-    //        }
-    //
-    //    }
-    
-    func sendFriendshipRequest(sender: String, receiver: String) {
-        
-
-        self.friendInviteReceived += [sender]
-        
-    }
-    
-    func acceptFriendshipRequest(sender: String, receiver: String) {
-        
-        
-        self.friendInviteReceived.removeAll { $0 == sender }
-        self.friendship += [receiver]
-    }
-    
-    func removeFriendship(username1: String, username2: String) {
-
-        self.friendship.removeAll { $0 == username2 }
-            
-    }
-    
-    func inviteRespond(username: String, event_id: String, accepted: Bool) {
-        if let index = self.events.firstIndex(where: { $0.id == event_id }) {
-            if accepted {
-                self.events[index].attendeesAccepted.append(username)
-                self.events[index].attendeesInvited.removeAll { $0 == username }
-            } else {
-                self.events[index].attendeesInvited.removeAll { $0 == username }
-            }
-        }
-    }
-    
-    func eventLeave(username: String, event_id: String) {
-        if let index = self.events.firstIndex(where: { $0.id == event_id }) {
-            self.events[index].attendeesAccepted.removeAll { $0 == username }
-        }
-    }
-    
-    func getNextEventID() -> String {
-        
-        if self.events.isEmpty {
-            return "1"
-        }
-        var IDs: [Int] = []
-        for event in self.events {
-            IDs.append(Int(event.id)!)
-        }
-        return String(IDs.max()! + 1)
-    }
-    
-    func createEvent(title: String, location: String, dateTime: Date, user: UserModel, invitees: [String]) {
-        
-        let nextID = getNextEventID()
-        
-        let new_event: EventModel = EventModel(id: nextID, title: title, location: location, dateTime: dateTime, attendeesAccepted: [user.email], attendeesInvited: invitees)
-        self.events += [new_event]
-    }
-    
-    func addUser(fullname: String, email: String, createdAt: Date) {
-        let newUser = UserModel(fullname: fullname, email: email, createdAt: Date())
-        self.users += [newUser]
-    }
+//    func sendFriendshipRequest(sender: String, receiver: String) {
+//        
+//
+//        self.friendInviteReceived += [sender]
+//        
+//    }
+//    
+//    func acceptFriendshipRequest(sender: String, receiver: String) {
+//        
+//        
+//        self.friendInviteReceived.removeAll { $0 == sender }
+//        self.friendship += [receiver]
+//    }
+//    
+//    func removeFriendship(username1: String, username2: String) {
+//
+//        self.friendship.removeAll { $0 == username2 }
+//            
+//    }
     
     func formatDate(date: Date) -> String {
         let dateFormatter = DateFormatter()
