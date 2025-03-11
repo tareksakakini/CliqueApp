d168876c6659cc2eb01e1b1ec69711924a26dce3
Tarek Sakakini <tarek.sakakini@gmail.com>
2025-03-11 14:00:29 -0700

    refactoring

--- CliqueApp/Models/Event.swift
+++ CliqueApp/Models/Event.swift
@@ -28,11 +28,4 @@ struct EventModel: Hashable {
         event.host = eventData["host"] as? String ?? ""
         return event
     }
-    
-    func sanityCheck(event: EventModel) -> Bool {
-        if self.title.count < 3 {
-            return false
-        }
-        return true
-    }
 }
